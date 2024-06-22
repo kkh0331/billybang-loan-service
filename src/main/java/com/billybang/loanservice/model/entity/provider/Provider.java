4606ce4d1fff257decb04d15e30a93029c845bbd
kihyun.kwon <kkh3272@naver.com>
2024-06-22 16:35:06 +0900

    refac: provider api 관련 기능에 mapper 적용

--- src/main/java/com/billybang/loanservice/model/entity/provider/Provider.java
+++ src/main/java/com/billybang/loanservice/model/entity/provider/Provider.java
@@ -1,8 +1,5 @@
 package com.billybang.loanservice.model.entity.provider;
 
-import com.billybang.loanservice.model.dto.provider.ProviderOverviewDto;
-import com.billybang.loanservice.utils.DateUtil;
-import com.billybang.loanservice.utils.NumberUtil;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.Date;
-import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -43,28 +39,5 @@ public class Provider {
     private Integer employeeCount;
 
     private String industryDetail;
-
-    public ProviderOverviewDto toProviderOverviewDto(Optional<FinStatement> statement){
-
-        String salesAmount = statement.map(finStatement -> NumberUtil.convertToBillion(finStatement.getSalesAmount())).orElse(null);
-        String businessProfit = statement.map(finStatement -> NumberUtil.convertToBillion(finStatement.getBusinessProfit())).orElse(null);
-        String netProfit = statement.map(finStatement -> NumberUtil.convertToBillion(finStatement.getNetProfit())).orElse(null);
-
-        return ProviderOverviewDto.builder()
-                .providerName(providerName)
-                .financialTier(financialTier)
-                .imgUrl(imgUrl)
-                .representativeName(representativeName)
-                .establishedAt(DateUtil.convertToKoreanDatePattern(establishedAt))
-                .providerSize(providerSize)
-                .providerType(providerType)
-                .salesAmount(salesAmount)
-                .businessProfit(businessProfit)
-                .netProfit(netProfit)
-                .creditLevel(creditLevel)
-                .employeeCount(NumberUtil.addCommas(employeeCount))
-                .industryDetail(industryDetail)
-                .build();
-    }
 
 }
