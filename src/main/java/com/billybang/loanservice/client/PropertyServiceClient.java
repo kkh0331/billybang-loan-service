283de96113bafd790320ae1b617d89cba3124fbf
kihyun.kwon <kkh3272@naver.com>
2024-06-24 15:36:01 +0900

    fix: 수정된 매물 서비스 feign rest api에 맞춰서 코드 수정

--- src/main/java/com/billybang/loanservice/client/PropertyServiceClient.java
+++ src/main/java/com/billybang/loanservice/client/PropertyServiceClient.java
@@ -4,12 +4,13 @@ import com.billybang.loanservice.api.ApiResult;
 import com.billybang.loanservice.model.dto.response.PropertyResDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "property-service", path = "/properties")
 public interface PropertyServiceClient {
 
-    @GetMapping("/area-price")
-    ApiResult<PropertyResDto> getPropertyInfo(@RequestParam("propertyId") Long propertyId);
+    @GetMapping("/{propertyId}/area-price")
+    ApiResult<PropertyResDto> getPropertyInfo(@PathVariable("propertyId") Long propertyId);
 
 }
