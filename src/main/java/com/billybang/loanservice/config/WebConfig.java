8bc73197a16d4251af2b070f8877b2a05722deb8
jinhyeon <vmflwu123@gmail.com>
2024-06-25 15:16:45 +0900

    fix: cors 설정 수정

--- src/main/java/com/billybang/loanservice/config/WebConfig.java
+++ src/main/java/com/billybang/loanservice/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Cookie")
                 .exposedHeaders("Authorization", "Cookie")
                 .allowCredentials(true);
